c856271f45f50d9c858c1ad989434f60e814b9d8
Ivan Zlatev <iazlatew@gmail.com>
2023-01-09 16:11:06 +0200

    delete some code

--- BASICS/First steps/introduction.js
+++
@@ -1,5 +0,0 @@
-function greeting(){
-console.log('Hello Ivan');
-console.log('Hello SOFTUNI');
-}
-greeting();
--- BASICS/First steps/readText(input)1.js
+++
@@ -1,5 +0,0 @@
-function readText(input){
-    let str = input[0];
-console.log(str);
-}
-readText(['SoftUni']);
