a487794cc722a10e0d09e40a5c42fa31cb14a437
Ivan Zlatev <iazlatew@gmail.com>
2023-01-31 13:19:45 +0200

    cut some not necessary code

--- BASICS/1.First steps/petShop(input).js
+++ BASICS/1.First steps/petShop(input).js
@@ -1,7 +1,5 @@
-function petShop(input){
-    let dog = input[0];
-    let cat = input[1];
-    let total = input[0]*2.50 +input[1]*4;
+function petShop(input) {
+    let total = input[0] * 2.50 + input[1] * 4;
     console.log(`${total} lv.`);
-    }
-petShop(['5','4']);
+}
+petShop(['5', '4']);
