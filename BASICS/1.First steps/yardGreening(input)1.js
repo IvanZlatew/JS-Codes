e39399ddbb9a5b675cce31cd75b26eebefac4cb3
Ivan Zlatev <iazlatew@gmail.com>
2023-01-31 13:26:03 +0200

    cut redundant code

--- BASICS/1.First steps/yardGreening(input)1.js
+++ BASICS/1.First steps/yardGreening(input)1.js
@@ -1,7 +1,6 @@
-function yardGreening(input){
-    let yard = input[0];
-    let finPrice = input[0]*7.61 - input[0]*7.61*0.18;
-    let discount = input[0]*7.61*0.18;
+function yardGreening(input) {
+    let finPrice = input[0] * 7.61 - input[0] * 7.61 * 0.18;
+    let discount = input[0] * 7.61 * 0.18;
     console.log(`The final price is: ${finPrice} lv.`);
     console.log(`The discount is: ${discount} lv.`);
 }
